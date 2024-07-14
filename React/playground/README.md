efc61ce6d3ae4b877487d405462c95698f138656
Chris Yoxall <chris.yoxall@gmail.com>
2024-07-14 13:37:52 +1200

    Rollback previous linting changes.

--- React/playground/README.md
+++ React/playground/README.md
@@ -30,5 +30,3 @@ export default {
 - Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
 - Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
 - Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
-
-Can add '--debug' to the lint script to see the rules being applied and the files checked.
--- React/playground/src/App.tsx
+++ React/playground/src/App.tsx
@@ -5,14 +5,17 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+    
+    let hobbies = ['Reading', 'Coding', 'Travelling', 'Cooking', 'Gardening'];
+  console.log(hobbies.findIndex(hobby => hobby === 'Coding'));
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
