216b8efabe7b8b565abdbc3cf546f0042ea348a2
Chris Yoxall <chris.yoxall@gmail.com>
2025-01-20 07:26:19 +1300

    Write embeddings to Postgress

--- Projects/Mistral/readme.md
+++ Projects/Mistral/readme.md
@@ -9,13 +9,123 @@ Set the API key in the environment variable `MISTRAL_API_KEY` before running the
 
     export MISTRAL_API_KEY=xxxxxxxx
 
+    or 
+
+    $Env:MISTRAL_API_KEY = "xxxxxxxx"
+
 
 ## Tutorial ##
 
 Much of this was taken from a Scrimba course https://v2.scrimba.com/intro-to-mistral-ai-c035
 
 
+## To Do ##
+
+Instead of calling the API, try running the a model locally. For example https://ollama.com/library/mixtral:8x7b
+
+Not just the LLM model, but also use a model from Hugging Space to generate the embeddings.
+
+
+Look into Dev Containers. They are a VS Code thing but I think the Jetbrains tools all support them:
+
+    - https://docs.github.com/en/codespaces/setting-up-your-project-for-codespaces/adding-a-dev-container-configuration/introduction-to-dev-containers
+    - https://code.visualstudio.com/docs/devcontainers/containers
+    - https://www.mikekasberg.com/blog/2021/11/06/what-are-dev-containers.html
+
+Elasticsearch has some interesting RAG / LLM articles: https://www.elastic.co/what-is/vector-embedding#get-started-with-vector-embedding-with-elasticsearch
+
+Learn about LLaMA (llama.cpp) and Ollama. Think Ollama is built on top of llama.cpp, using it as its inference backend and adding a user friendly
+layer that makes it much easier to use:
+
+    - https://github.com/ggerganov/llama.cpp
+    - https://namrata23.medium.com/run-llms-locally-or-in-docker-with-ollama-ollama-webui-379029060324  Am interested in running it in Docker if possible
+
+Some RAG examples using Postgress & the Llama model to generate embeddings:
+
+    - https://pgdash.io/blog/rag-with-postgresql.html
+    - https://www.enterprisedb.com/blog/rag-app-postgres-and-pgvector
+    - https://www.enterprisedb.com/blog/what-is-pgvector
+    - https://www.enterprisedb.com/blog/what-is-pgvector
+
+
+Think I'd much rather write this in Python rather than JavaScript / Node
+
+
+
 ## Retrieval-Augmented Generation (RAG) ##
 
-RAG is used to enhance AI language models by combining specified knowledge them with external knowledge retrieval.
+RAG is used to enhance AI language models by combining specified knowledge with external knowledge retrieval.
+
+Embeddings are a way to represent words, sentences, or documents as vectors in a high-dimensional space. The purpose of embeddings
+is to capture the semantic meaning of text, such that words or phrases so that similar meanings are located close to each other in this
+vector space. For example 'dog' and 'puppy' should be closer together than 'dog' and 'cat', which in turn should be closer together
+than 'dog' and 'spaceship'.
+
+Embedding is a mathematical concept that refers to placing one object into a different space. The vectors used for embeddings often
+have hundreds or thousands of dimensions, each refering to a different semantic or contextual meaning of the word/phrase.
+
+You need to split larger pieces of text into smaller chunks as a large number of words (i.e. a full book) doesn't have a specific
+semantic meaning.
+
+
+## Postgress ##
+
+Run Postgres in Docker:
+
+    docker run --name embeddings --rm -e POSTGRES_PASSWORD=secretpassword -d -p 5432:5432 postgres
+
+Shell into Postgres container:
+
+    docker exec -it embeddings /bin/bash
+
+Check Postgress version:
+
+    psql --version
+
+Install Git & tools to build pgvector (note version number for Postress development files):
+
+    apt-get update && apt-get install git build-essential postgresql-server-dev-17
+
+Compile & install the pgvector Postress extension (as per https://github.com/pgvector/pgvector):
+
+    cd /tmp
+    git clone --branch v0.8.0 https://github.com/pgvector/pgvector.git # note version number
+    cd pgvector
+    make
+    make install
+
+Connect to Postgress instance using 'psql' (likely not prompted for password inside container):
+
+    psql -h localhost -p 5432 -U postgres
+
+Create database:
+
+    CREATE DATABASE embeddemo;
+
+Create new user so don't need superuser password:
+
+    CREATE ROLE embeduser WITH LOGIN PASSWORD 'pa55w0rd';
+
+Change to the 'embeddemo' database:
+
+    \c embeddemo
+
+Enable the pgvector extension. Once enabled you can confirm its enabled by doing '\dx':
+
+    CREATE EXTENSION vector;
+
+Create a table to store the handbook documents. The number after Vector is the number of dimensions in the generated vector which may change
+if using a different model to generate the vectors:
+
+    CREATE TABLE handbook_docs (id bigserial primary key, content text, embedding vector(1024));
+
+Grant the 'embeduser' user access to the table:
+
+    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE handbook_docs TO embeduser;
+
+As using 'bigserial' type for 'id' need sequence permissions:
+
+    GRANT USAGE, SELECT ON SEQUENCE handbook_docs_id_seq TO embeduser;
+
+
 
--- Projects/Mistral/split_handbook.mjs
+++ Projects/Mistral/split_handbook.mjs
@@ -3,6 +3,7 @@
 import fs from 'node:fs/promises';
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { Mistral } from '@mistralai/mistralai';
+import pg from 'pg';
 
 async function readTextFile(filePath) {
     try {
@@ -15,7 +16,7 @@ async function readTextFile(filePath) {
 // Simplest method to split text is CharacterTextSplitter https://js.langchain.com/v0.1/docs/modules/data_connection/document_transformers/character_text_splitter/
 // The recommended text splitter for general text is RecursiveCharacterTextSplitter https://js.langchain.com/v0.1/docs/modules/data_connection/document_transformers/recursive_text_splitter/
 
-const data = await readTextFile(`handbook.txt`);
+const doc = await readTextFile(`handbook.txt`);
 
 /* Want to split the source into chunks where each chunk deals with a single topic as it will create better
 embeddings (better vectors). In general:
@@ -24,34 +25,108 @@ embeddings (better vectors). In general:
     - longer chunks capture wider context but produce too broad a scope
 
 This means you often ideally want the smallest size without loosing context. Can only work out the 'best' option
-through experimentation, and it will vary between different underlying data being chunked. */
+through experimentation, and it will vary between different underlying data being chunked. 
 
-const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 250,
-    chunkOverlap: 40
+Note: Do any of the splitters break up text by sentance or paragraph? That seems more logical than guessing lengh of charcters. */
+
+async function splitDocument(doc) {
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 250,
+        chunkOverlap: 40
+    });
+
+    const output = await splitter.createDocuments([doc]);
+    const txtArray = output.map(chunk => chunk.pageContent);
+    return txtArray;
+}
+
+const docChunks = await splitDocument(doc);
+
+//console.log(docChunks);
+
+async function createEmbeddings(chunks){
+
+    const apiKey = process.env.MISTRAL_API_KEY || "xxxxxxxx";
+    const client = new Mistral({apiKey: apiKey});
+    
+    const embeddings = await client.embeddings.create({
+        model: 'mistral-embed',
+        inputs: chunks
+    });
+
+    // Create a new array where each item is the chunk and its calculated embedding
+    const result = chunks.map((chunk, i) => {
+        return {
+            content: chunk,
+            embedding: embeddings.data[i].embedding
+        }
+    });
+
+    return result;
+}
+
+const embeddings = await createEmbeddings(docChunks);
+
+// const embeddings = [
+//     { content: 'This is the first chunk of text.', embedding: [0.1, 0.2, 0.3] },
+//     { content: 'This is the second chunk of text.', embedding: [0.4, 0.5, 0.6] },
+//     { content: 'This is the third chunk of text.', embedding: [0.7, 0.8, 0.9] },
+//     { content: 'This is the fourth chunk of text.', embedding: [0.10, 0.11, 0.12] },
+//     { content: 'This is the fifth chunk of text.', embedding: [0.13, 0.14, 0.15] },
+//     { content: 'This is the sixth chunk of text.', embedding: [0.16, 0.17, 0.18] },
+// ]
+
+// Display one item from result
+console.log(embeddings[5]);
+
+// Connect to a PostgreSQL database to store the embeddings. Ensure the settings
+// here match the settings for your PostgreSQL database. See the 'readme'.
+
+const { Pool } = pg
+
+const pool = new Pool({
+    user: 'embeduser',
+    host: 'localhost',
+    database: 'embeddemo',
+    password: 'pa55w0rd',
+    port: 5432,
+    idleTimeoutMillis: 300
 });
 
-const output = await splitter.createDocuments([data]);
 
-// Output is an array of objects, each object is a chunk of text
-const index = 35;
-//console.log(output[index]);
-//console.log(output[index].metadata);
+async function storeEmbeddings(embeddings) {
 
-// Only need the pageContent field
-const txtArray = output.map(chunk => chunk.pageContent);
-console.log(txtArray[index]);
+    const client = await pool.connect();
 
-const apiKey = process.env.MISTRAL_API_KEY;
-const client = new Mistral({apiKey: apiKey});
+    try {
 
-//Get the embeddings for one chunk
-const embeddingsResponse = await client.embeddings.create({
-    model: 'mistral-embed',
-    inputs: txtArray[index],
-});
+        await client.query('BEGIN');
 
-console.log(embeddingsResponse);
-console.log(embeddingsResponse.data[0].embedding);
+        const insertQuery = `
+            INSERT INTO handbook_docs (content, embedding)
+            VALUES ($1, $2)
+        `;
 
-// Create a new array where each item is the chunk and its calculated embedding
+        for (const { content, embedding } of embeddings) {
+            // Need to format vector as string that starts with '[' and ends with ']'.
+            const vectorString = `[${embedding.join(',')}]`
+            // console.log("Inserting:", content);
+            await client.query(insertQuery, [content, vectorString]);
+        }
+
+        await client.query('COMMIT');
+
+    } catch (err) {
+        
+        await client.query('ROLLBACK');
+        console.error('Failed to store embeddings:', err);
+
+    } finally {
+        client.release();
+    }
+}
+
+
+await storeEmbeddings(embeddings);
+
+console.log("Done!")
